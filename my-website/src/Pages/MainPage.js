aa81c64dd57ca78a4ba98f55f86fbba019416fd9
ScriptKidHicks <Tammashicks@gmail.com>
2021-09-20 18:00:37 -0700

    Modified card size

--- my-website/src/Pages/MainPage.js
+++ my-website/src/Pages/MainPage.js
@@ -13,12 +13,18 @@ function MainPage() {
     <div className={classes.main}>
       <img src={profilePic} alt="Tammas Hicks" className={classes.profile} />
       <div className={classes.introduction}>
-        Hi! I'm Tammas, a Programmer currently based out of Eugene, Oregon. I'm
-        currently attending the UO for a degree in computer information science.
-        I'm working towards a career in front end development, and over the last
-        two months I've been learning React. I'm interested in the ways we can
-        use programming and technology to tell stories, and to improve the lives
-        of the most underserved among us.
+        <div className={classes.greetingTag}>Hi!</div>
+        <div className={classes.dash}>
+          <hr />
+        </div>
+        <p className={classes.introductionText}>
+          I'm Tammas, a Programmer based out of Eugene, Oregon. I'm currently
+          attending the UO for a degree in computer information science. I'm
+          working towards a career in front end development, and over the last
+          two months I've been learning React. I'm interested in the ways we can
+          use programming and technology to tell stories, and to improve the
+          lives of the most underserved among us.{" "}
+        </p>
       </div>
       <div className={classes.projectsTitle}>--Projects--</div>
       <div>
