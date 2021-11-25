4db19945d29c1e412a133186910e429724db97f4
ScriptkidHicks <tammashicks@gmail.com>
2021-11-25 12:42:45 -0800

    test

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -13,55 +13,39 @@ import ThisWebsite from "./Pages/ThisWebsite";
 import SudokuSolver from "./Pages/SudokuSolver";
 import SubredditWrapper from "./Pages/SubredditWrapper";
 
-import HeaderBarMobile from "./HeaderFooter/HeaderBarMobile";
-import MainMobile from "./MobilePages/MainMobile";
-
 function App() {
-  if (window.screen.width <= 1024) {
-    return (
-      <div>
-        <HeaderBarMobile />
-        <Switch>
-          <Route path="/" exact>
-            <MainMobile />
-          </Route>
-        </Switch>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <HeaderBar />
-        <Switch>
-          <Route path="/" exact>
-            <MainPage />
-          </Route>
-          <Route path="/AboutMe">
-            <AboutMe />
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-          <Route path="/ColevilleBot">
-            <ColevilleBot />
-          </Route>
-          <Route path="/Y86-64">
-            <Y86_64PY />
-          </Route>
-          <Route path="/ThisWebsite">
-            <ThisWebsite />
-          </Route>
-          <Route path="/SudokuSolver">
-            <SudokuSolver />
-          </Route>
-          <Route path="/SubredditWrapper">
-            <SubredditWrapper />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <HeaderBar />
+      <Switch>
+        <Route path="/" exact>
+          <MainPage />
+        </Route>
+        <Route path="/AboutMe">
+          <AboutMe />
+        </Route>
+        <Route path="/Contact">
+          <Contact />
+        </Route>
+        <Route path="/ColevilleBot">
+          <ColevilleBot />
+        </Route>
+        <Route path="/Y86-64">
+          <Y86_64PY />
+        </Route>
+        <Route path="/ThisWebsite">
+          <ThisWebsite />
+        </Route>
+        <Route path="/SudokuSolver">
+          <SudokuSolver />
+        </Route>
+        <Route path="/SubredditWrapper">
+          <SubredditWrapper />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- my-website/src/MobilePages/MainMobile.js
+++
@@ -1,7 +0,0 @@
-import classes from "../MobileCss/MainMobile.module.css";
-
-function MainMobile() {
-  return <div className={classes.main}></div>;
-}
-
-export default MainMobile;
