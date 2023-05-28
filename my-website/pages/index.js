1d0c150a98f9bbbade7fb3e46637828a882788bf
Tammas Hicks <tammashicks@gmail.com>
2023-05-28 12:38:56 -0700

    attempting a flow push

--- my-website/pages/index.js
+++ my-website/pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
       <TitleSection></TitleSection>
       <AboutSection></AboutSection>
       <ProjectsSection></ProjectsSection>
+
       <FooterBar></FooterBar>
     </main>
   );
