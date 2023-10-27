f9e9015452ed3b22798464c9442a8ba6331ab6ba
Lillith Hicks <tammashicks@gmail.com>
2023-10-26 18:13:53 -0700

    updating build method

---
+++ my-website/out/_next/static/qH6OgendVKBXHeBh0MjHU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/247-3863b3ef695da6bf.js","static/css/e7d3bab42d9af29d.css","static/chunks/pages/index-82da10cb9c0e10eb.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
